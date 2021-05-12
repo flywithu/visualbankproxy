9b434242c28bfdc04317e26707f32d32b021cf88
flywithu <34819252+flywithu@users.noreply.github.com>
2021-05-12 17:14:06 +0900

    Update main.yml

--- app/src/main/java/com/flywithu/forvisualbank/gennoti/KBPush_Gen.java
+++ app/src/main/java/com/flywithu/forvisualbank/gennoti/KBPush_Gen.java
@@ -4,13 +4,14 @@ import com.flywithu.forvisualbank.parser.SpendInfo;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class KBPush_Gen implements  MessageGenerator{
     //String msg = "05/11 18:14 \n1111 \n호식이 \n체크카드출금 1,000원 \n잔액 2,000원";
 
     @Override
     public String genMessage(SpendInfo si) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd HH:mm");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd HH:mm", Locale.KOREA);
         DecimalFormat wonformat = new DecimalFormat("###,###,###원");
         return String.format("%s \n%s \n%s \n%s %s \n잔액 %s",
                 dateFormat.format(si.getDateTime()),
